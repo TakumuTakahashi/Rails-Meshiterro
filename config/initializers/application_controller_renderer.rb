904ecac76b74b7143624fbdef93818e29c7d8716
Takahashi Takumu <takumu.0213@icloud.com>
2020-02-21 20:09:15 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#original
+Refile.secret_key = '35b9fb08983455c6d98f082c483aefd094470232779b38599e7df4b315ca7e6a6d168f5e51b0aa9dc9d6efa036bc6a7e87f0705a818e83f37118187c21ca7aa1'
