e1124b4a6c21df92371ff690446886e33ef28055
Takahashi Takumu <takumu.0213@icloud.com>
2020-02-22 18:22:13 +0900

    [Finish] 5-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   root 'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :post_images, only: [:new, :create, :index, :show] do
-    resource :post_comments, only: [:create]
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
   end
 
 end
